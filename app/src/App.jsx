a51edd10cb622eeffc5c80ef8ec500f717c97a22
harsha <fathimaharshaummatt@gmail.com>
2024-10-17 18:28:02 +0530

    ToastContainer

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router,Route, Routes } from "react-router-dom"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "./Component/Navbar"
 import Footer from "./Component/Footer"
 import Home from "./Pages/Home"
@@ -8,6 +10,7 @@ const App = () => {
   return (
     <>
       <Router>
+      <ToastContainer/>
       <Navbar/>
        <Routes>
         <Route path="/" element={<Home/>}/>
