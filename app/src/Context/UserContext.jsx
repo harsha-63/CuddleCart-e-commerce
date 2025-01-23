2a41f3f3ebcebe4beebde0b5a61ba028fd65f9e7
harsha <fathimaharshaummatt@gmail.com>
2025-01-23 16:33:40 +0530

    updated

--- app/src/Context/UserContext.jsx
+++ app/src/Context/UserContext.jsx
@@ -56,14 +56,15 @@ const UserProvider = ({ children }) => {
   const loginUser = async (email, password) => {
     try {
       const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, { email, password }, { withCredentials: true });
-      Cookies.set("currentUser",data.currentUser)
+      console.log(data)
+      Cookies.set("currentUser",JSON.stringify(data.currentUser))
       const user = Cookies.get("currentUser");
       setCurrentUser(JSON.parse(user));
       Cookies.set("token", data.token); 
       toast.success("Logged in successfully");
     } catch (err) {
-      toast.error("Invalid credentials or error occurred");
-      throw err;  
+      toast.error(axiosErrorManager(err));
+  
     }
   };
 
